eeb7a4e4c083227f79b83e43293d67765dbddc82
Andrew Brennan <amb42513@gmail.com>
2021-04-10 12:04:49 -0400

    Latest Updates to "Level1" Scene
    - Added some mouse controls for moving data around in the data structure
    - Added some other assests (scripts, sprites, etc.)
    - Organized the assets folder

---
+++ Unity Group Project/Assets/Scripts/Object_Scripts/Boundaries.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    public Camera MainCamera;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = Camera.main;
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<Collider2D>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<Collider2D>().bounds.extents.y; //etents = size of height / 2
+    }
+
+    // LateUpdate is called once per frame
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, (screenBounds.x * -1 + objectWidth), screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, (screenBounds.y * -1 + objectHeight), screenBounds.y - objectHeight);
+        transform.position = viewPos;
+    }
+}
