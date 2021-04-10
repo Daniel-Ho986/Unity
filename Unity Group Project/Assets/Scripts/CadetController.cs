b7ce1d48ca9b9209e588a08973c5e39dc145c8f9
BuildTools <rajeev.basanta@gmail.com>
2021-04-10 14:07:21 -0400

    Changes to player controls and assets
    -Added background music
    -Added horizontal and vertical player controls
    -Added rigidbody and box collider components to the player
    -Added invisible platform  for the player to walk and jump on

--- Unity Group Project/Assets/Scripts/CadetController.cs
+++ Unity Group Project/Assets/Scripts/CadetController.cs
@@ -8,6 +8,11 @@ public class CadetController : MonoBehaviour
 	[SerializeField] Rigidbody2D rigid;
 	[SerializeField] float speed = 5f;
 	[SerializeField] bool isFacingRight = true;
+	[SerializeField] float jumpForce = 250f;
+	[SerializeField] LayerMask whatIsGround;
+	[SerializeField] float groundDistance = 1.5f;
+	[SerializeField] bool grounded;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +26,11 @@ public class CadetController : MonoBehaviour
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
+		if(Input.GetButtonDown("Jump"))
+		{
+			Jump();
+		}
+		
     }
 	
 	//called potentially multiple times per frame, best for physics for smooth behavior
@@ -40,5 +50,33 @@ public class CadetController : MonoBehaviour
 		transform.localScale = playerScale;
 		
 		isFacingRight = !isFacingRight;
+	}
+	
+	void Jump()
+	{
+		if (isOnGround())
+		{
+			rigid.velocity = new Vector2 (rigid.velocity.x, 0);
+			rigid.AddForce(new Vector2(0, jumpForce));
+		}
+		
+	}
+	bool isOnGround()
+	{
+		Vector2 position = transform.position;
+		Vector2 direction = Vector2.down;
+		
+		Debug.DrawRay(position, direction, Color.green);
+		
+		RaycastHit2D hit = Physics2D.Raycast(position, direction, groundDistance, whatIsGround);
+		if (hit.collider == null)
+		{
+			grounded = false;			
+		}
+		else
+		{
+			grounded = true;
+		}
+		return grounded;
 	}
 }
