73244466faddf62e6932fe4052a3ba92f04cd56c
Daniel-Ho986 <dhonaruto@gmail.com>
2021-04-12 19:31:20 -0400

    Implemented Volume Adjustment

---
+++ Unity Group Project/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public GameObject ObjectMusic;
+
+    //value from the slider, and it converts to volume level
+    private float musicVolume = 1f;
+    private AudioSource AudioSource;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ObjectMusic = GameObject.FindWithTag("GameMusic");
+        AudioSource = ObjectMusic.GetComponent<AudioSource>();
+
+        //Set Volume
+        musicVolume = PlayerPrefs.GetFloat("volume");
+        AudioSource.volume = musicVolume;
+        volumeSlider.value = musicVolume;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        AudioSource.volume = musicVolume;
+        PlayerPrefs.SetFloat("volume", musicVolume);
+    }
+
+    public void updateVolume(float volume){
+        musicVolume = volume;
+    }
+}
