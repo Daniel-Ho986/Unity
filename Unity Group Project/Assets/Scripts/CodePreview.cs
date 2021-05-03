6a0151ef85d51ce934afcb721c12e1de2104bafb
Daniel-Ho986 <dhonaruto@gmail.com>
2021-05-02 22:50:45 -0400

    implemented bubble sort in java

---
+++ Unity Group Project/Assets/Scripts/CodePreview.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CodePreview : MonoBehaviour
+{
+    public TextMeshProUGUI code;
+
+    public void SetJava(){
+        code.text = "void bubbleSort(int arr[]) { \n int n = arr.length; \n for (int i = 0; i < n-1; i++) \n for (int j = 0; j < n-i-1; j++) \nif (arr[j] > arr[j+1]){ \n\n // swap arr[j+1] and arr[j] \n int temp = arr[j]; \n arr[j] = arr[j+1]; \n arr[j+1] = temp; \n }";
+            
+    }
+}
